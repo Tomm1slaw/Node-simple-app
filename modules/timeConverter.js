5851fe27298f681a6cb84a9224d4b1b6a54e5fc2
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-08-25 15:31:26 +0200

    Fix bug timeConverter.js

--- modules/timeConverter.js
+++ modules/timeConverter.js
@@ -5,13 +5,16 @@ function converter(uptime){
 	var sec = Math.floor(uptime%60);
 	var hour = Math.floor(min/60);
 	var minLeft = (min%60);
+	var time;
 
 	if (min < 60) {
-    var  time =  min + 'min and ' + sec + 'sec.';
-    } else if (min >= 60) {
-    var  time =  hour + 'h and ' + minLeft + 'min and ' + sec + 'sec.';
+    time =  min + 'min. ' + sec + 'sek.';
+    } else {
+    time =  hour + 'godz. ' + minLeft + 'min. ' + sec + 'sek.';
     }
     return time;
 }
+
+converter(3700);
 
 exports.convert = converter;
